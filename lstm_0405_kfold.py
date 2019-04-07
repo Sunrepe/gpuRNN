343dfc70bc65550b293534b3744bb0230cccb405
Sunrepe <email@example.combnuzxf@163.com>
2019-04-07 13:00:01 +0800

    kfoldtestlstm

--- lstm_0405_kfold.py
+++ lstm_0405_kfold.py
@@ -23,12 +23,12 @@ max_seq = 800
 # Training
 learning_rate = 0.0025
 lambda_loss_amount = 0.0015
-training_iters = 200  # Loop 1000 times on the dataset
+training_iters = 150  # Loop 1000 times on the dataset
 batch_size = 100
 display_iter = 1000  # To show test set accuracy during training
 model_save = 80
 
-k_fold_num = 1
+k_fold_num = 0
 savename = '_LSTM_kfold'+str(k_fold_num)
 LABELS = ['double', 'fist', 'spread', 'six', 'wavein', 'waveout', 'yes', 'no', 'finger', 'snap']
 
