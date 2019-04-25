3083d2b8d89746a379ca5730bbba8da0e4d95c12
Sunrepe <email@example.combnuzxf@163.com>
2019-04-25 21:05:24 +0800

    combine_offline_f1

--- feature_0get.py
+++ feature_0get.py
@@ -29,8 +29,8 @@ batch_size = 400
 display_iter = 4000  # To show test set accuracy during training
 model_save = 20
 
-k_fold_num = 2
-feature_num__s = 1
+k_fold_num = 4
+feature_num__s = 0
 fold = './data/actdata/'
 savename = '_feature{}_kfold{}'.format(feature_num__s, k_fold_num)
 LABELS = ['double', 'fist', 'spread', 'six', 'wavein', 'waveout', 'yes', 'no', 'finger', 'snap']
@@ -192,7 +192,7 @@ def main():
     y = tf.placeholder(tf.float32, [None, n_classes])
     seq_len = tf.placeholder(tf.float32, [None])
 
-    pred = LSTM_RNN_f1(x, seq_len)
+    pred = LSTM_RNN_f0(x, seq_len)
 
     # Loss, optimizer and evaluation
     l2 = lambda_loss_amount * sum(
