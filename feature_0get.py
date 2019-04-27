13ffa396df03ea5e7a337f3a97384f914912e4ce
Sunrepe <email@example.combnuzxf@163.com>
2019-04-27 08:03:31 +0800

    combine_offline_f1

--- feature_0get.py
+++ feature_0get.py
@@ -29,7 +29,7 @@ batch_size = 400
 display_iter = 4000  # To show test set accuracy during training
 model_save = 20
 
-k_fold_num = 3
+k_fold_num = 4
 feature_num__s = 1
 fold = './data/actdata/'
 savename = '_feature{}_kfold{}'.format(feature_num__s, k_fold_num)
