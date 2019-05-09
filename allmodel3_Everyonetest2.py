6a8ed7cb16a27f0e956c524f524b677c33db0453
Sunrepe <email@example.combnuzxf@163.com>
2019-05-09 16:40:08 +0800

    all_3testevery

--- allmodel3_Everyonetest2.py
+++ allmodel3_Everyonetest2.py
@@ -1,6 +1,9 @@
 '''
 使用Model3进行所有人群测试。
-此处获得所有人的识别结果。
+结果输出到针对每个个体，仅输出用于判断的算法的最终结果
+与all_lstm1进行比较实验。
+数据转存到data/res10/all_lstm3/中
+注意需要保存标签和网络输出。
 '''
 # 用于测试新数据是否和老数据可以共用
 from data_pre.dataFortest import *
@@ -28,15 +31,12 @@ model_name = "E:/Research-bachelor/storeMODELs/all_lstm_3/all_model3_kfold{}/" \
 foldname = './data/actdata/'
 matrix_save_path = "E:/Research-bachelor/storeMODELs/all_lstm_3/all_model3Matrix_kfold{}.txt".format(k_fold_num)
 
-df_save_path = 'E:/Research-bachelor/storeMODELs/all_lstm_3/allmodel3_everyone_precision_kfold{}.csv'.format(k_fold_num)
-df_save_path2 = 'E:/Research-bachelor/storeMODELs/all_lstm_3/allmodel3_everyone_recall_kfold{}.csv'.format(k_fold_num)
-df_save_path3 = 'E:/Research-bachelor/storeMODELs/all_lstm_3/allmodel3_everyone_f1score_kfold{}.csv'.format(k_fold_num)
-
 # Training
 learning_rate = 0.0025
 lambda_loss_amount = 0.0015
 LABELS = ['double', 'fist', 'spread', 'six', 'wavein', 'waveout', 'yes', 'no', 'finger', 'snap']
 tmp_use_len = [150, 150, 250, 450, 800, 800, 800, 800, 400]
+
 
 def Matrix_to_CSV(filename, data):
     with open(filename, "a", newline='', ) as csvfile:
@@ -142,8 +142,7 @@ def LSTM_RNN_tmp(x0,x1,x2,x3,x4,x5,x6,x7,x8,
         lstm_out8 = tf.nn.dropout(lstm_out8, keep_prob=0.5)
         lstm_out8 = tf.layers.dense(lstm_out8, 10)
 
-    return [lstm_out0,lstm_out1,lstm_out2,lstm_out3,lstm_out4,lstm_out5,lstm_out6,lstm_out7,lstm_out8]
-    # return tf.matmul(lstm_out, _weight['out']) + _bias['out']
+    return lstm_out0+lstm_out1+lstm_out2+lstm_out3+lstm_out4+lstm_out5+lstm_out6+lstm_out7+lstm_out8
 
 
 def main():
@@ -189,30 +188,14 @@ def main():
     # df_data = []
     print("Start test!")
     person_list = getPersons_every(foldname, k_fold_num)
-    print("persons:", person_list)
-    every_stream_matrix = {}
-    for i_stram in range(10):
-        every_stream_matrix[i_stram] = np.zeros([10,10])
-    every_stream_precision = {}
-    for i_stram in range(10):
-        every_stream_precision[i_stram] = []
-    every_stream_recall = {}
-    for i_stram in range(10):
-        every_stream_recall[i_stram] = []
-    every_stream_f1score = {}
-    for i_stram in range(10):
-        every_stream_f1score[i_stram] = []
-    # every_precision = {1:[],2:[],3:[]}
-    # every_recall = {1:[],2:[],3:[]}
-    # every_f1score = {1:[],2:[],3:[]}
+
     for person in person_list:
         print(person)
         test_sets = perAll_data_merge(foldname=foldname,
                                       max_seq=max_seq,
                                       num_class=n_classes,
                                       testperson=person)
-
-        result_labels = test_sets.all_label.argmax(1)
+        result_labels = test_sets.all_label
         feed_dic = {
             y: test_sets.all_label,
             x0: test_sets.data[0],
@@ -239,55 +222,8 @@ def main():
             preds,
             feed_dict=feed_dic
         )
-        for i_pre in range(9):
-            pre_onehot = one_hot_predictions[i_pre]
-            predictions = pre_onehot.argmax(1)
-            _percision = metrics.precision_score(result_labels, predictions, average="weighted")
-            _recall = metrics.recall_score(result_labels, predictions, average="weighted")
-            _f1Score = metrics.f1_score(result_labels, predictions, average="weighted")
-            every_stream_precision[i_pre].append(_percision)
-            every_stream_recall[i_pre].append(_recall)
-            every_stream_f1score[i_pre].append(_f1Score)
-            # matrix
-            confusion_matrix = metrics.confusion_matrix(result_labels, predictions)
-            # print(confusion_matrix)
-            every_stream_matrix[i_pre] += confusion_matrix
-
-        predictions = sum(one_hot_predictions).argmax(1)
-        _percision = metrics.precision_score(result_labels, predictions, average="weighted")
-        _recall = metrics.recall_score(result_labels, predictions, average="weighted")
-        _f1Score = metrics.f1_score(result_labels, predictions, average="weighted")
-        # zhengcun
-        every_stream_precision[9].append(_percision)
-        every_stream_recall[9].append(_recall)
-        every_stream_f1score[9].append(_f1Score)
-
-        print("Precision: {}%".format(100 * _percision))
-        print("Recall: {}%".format(100 * _recall))
-        print("f1_score: {}%".format(100 * _f1Score))
-        # print("Precision: ",metrics.precision_score(result_labels, predictions, average=None))
-        # # print("Recall: {}%".format(100 * metrics.recall_score(result_labels, predictions)))
-        # # print("f1_score: {}%".format(100 * metrics.f1_score(result_labels, predictions)))
-
-        print("{}'s Confusion Matrix:".format(person))
-        confusion_matrix = metrics.confusion_matrix(result_labels, predictions)
-        print(confusion_matrix)
-        every_stream_matrix[9] += confusion_matrix
-    print("All stream matrix:")
-    for i_st in range(10):
-        print(every_stream_matrix[i_st])
-        every_stream_matrix[i_st] = every_stream_matrix[i_st].astype('int32')
-        Matrix_to_CSV(matrix_save_path, every_stream_matrix[i_st])
-    # person_list.append('allperson_kfold{}'.format(k_fold_num))
-    # df = pd.DataFrame(every_stream_precision[0], index=person_list, columns=["S"])
-    df1 = pd.DataFrame(every_stream_precision, index=person_list)
-    df2 = pd.DataFrame(every_stream_recall, index=person_list)
-    df3 = pd.DataFrame(every_stream_f1score, index=person_list)
-    # df = pd.DataFrame(df_data, index=person_list, columns=["Recall","Precision","F1_score","Total_wrong"])
-
-    df1.to_csv(df_save_path)
-    df2.to_csv(df_save_path2)
-    df3.to_csv(df_save_path3)
+        Matrix_to_CSV('data/res10/all_lstm3/pre_{}'.format(person), one_hot_predictions)
+        Matrix_to_CSV('data/res10/all_lstm3/label_{}'.format(person), one_hot_predictions)
 
     sess.close()
 
